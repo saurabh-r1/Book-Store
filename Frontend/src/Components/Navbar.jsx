103597b82acef13fd7be41223915fdaac793c514
Saurabh <saurabhsingh99r@gmail.com>
2025-10-27 21:18:20 +0530

    First Commit

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 function Navbar() {
+  const navItems=(
+    <>
+    <li><a>Home</a></li>
+        <li><a>Course</a></li>
+        <li><a>Contact</a></li>
+        <li><a>About</a></li>
+      </>
+  );
   return (
-    <div>
+    
+    <div max-w-screen-2xl container mx-auto md:px-20 px-4>
         <div>
             <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
@@ -13,40 +22,26 @@ function Navbar() {
       <ul
         tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {navItems}
+        
       </ul>
     </div>
-    <a className="text-2xl font-bold">Book Store</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
+    <a className="text-2xl font-bold cursor-pointer">Book Store</a>
   </div>
   <div className="navbar-end">
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      {navItems}
+    
+    </ul>
+  </div>
+  {/* <div className="navbar-end"> */}
     <a className="btn">Button</a>
   </div>
 </div>
         </div>
-    </div>
+        </div>
+    // </div>
   )
 }
 
